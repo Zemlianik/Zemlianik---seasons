7f1e7468cf629c852757f601ea33ff0e4965bdcd
Aleksandr Zemlianik <109904887+Zemlianik@users.noreply.github.com>
2022-09-10 10:57:45 +0300

    импорт и функ. ввода стр

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app=Flask(__name__)
 
 @app.route('/')
 def index():
     return '1x4'
+
+@app.route('/let')
+def leto():
+    return render_template('let.html')
+
+@app.route('/osen')
+def osen():
+    return render_template('osen.html')
+
+@app.route('/vesn')
+def vesna():
+    return render_template('vesn.html')
+
+@app.route('/wint')
+def winter():
+    return render_template('wint.html')
 
 if __name__=='__main__':
     app.run(debug=True)
